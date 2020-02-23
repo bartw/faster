61cd0087461dd2339a98a1690fe7c9e44d29a2f9
Bart Wijnants <bart.w@sparkcentral.com>
2020-02-23 15:47:05 +0100

    title link

--- src/layout.js
+++ src/layout.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Nav from "./nav";
 
 const Layout = ({ loading, children }) => (
   <div className="p-4">
     <header>
-      <h1 className="text-5xl font-extrabold">faster</h1>
+      <Link to="/">
+        <h1 className="text-5xl font-extrabold">faster</h1>
+      </Link>
     </header>
     {!loading && (
       <>
