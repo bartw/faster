617bb69fe03262838f323e204a15e906a3fab907
Bart Wijnants <bart.w@sparkcentral.com>
2020-02-23 14:17:48 +0100

    fix test description

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./app";
 
-test("renders learn react link", () => {
+test("renders title", () => {
   const { getByText } = render(<App />);
   const titleElement = getByText(/faster/i);
   expect(titleElement).toBeInTheDocument();
